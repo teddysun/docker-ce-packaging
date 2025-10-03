1dba112b4a0a302bfc4e4ac1f330ecf50969ce65
Teddysun <i@teddysun.com>
2025-10-03 10:07:08 +0900

    Bump Docker version to v28.5.0 Bump model version to v0.1.42
    Signed-off-by: Teddysun <i@teddysun.com>

--- common.mk
+++ common.mk
@@ -17,10 +17,10 @@ CHOWN:=docker run --rm -v $(CURDIR):/v -w /v alpine chown
 DEFAULT_PRODUCT_LICENSE:=Community Engine
 PACKAGER_NAME?=
 DOCKER_GITCOMMIT:=abcdefg
-GO_VERSION:=1.25.0
+GO_VERSION:=1.25.1
 PLATFORM=Docker Engine - Community
 SHELL:=/bin/bash
-VERSION?=28.4.0
+VERSION?=28.5.0
 
 # DOCKER_CLI_REPO and DOCKER_ENGINE_REPO define the source repositories to clone
 # the source from. These can be overridden to build from a fork.
@@ -36,7 +36,7 @@ DOCKER_MODEL_REPO   ?= https://github.com/docker/model-cli.git
 # tagged a release with the same version.
 #
 # For other situations, specify DOCKER_CLI_REF and/or DOCKER_ENGINE_REF separately.
-REF                ?= v28.4.0
+REF                ?= v28.5.0
 DOCKER_CLI_REF     ?= $(REF)
 DOCKER_ENGINE_REF  ?= $(REF)
 # DOCKER_COMPOSE_REF is the version of compose to package. It usually is a tag,
@@ -47,7 +47,7 @@ DOCKER_COMPOSE_REF ?= v2.39.4
 DOCKER_BUILDX_REF  ?= v0.29.0
 # DOCKER_MODEL_REF is the version of model to package. It is usually a tag,
 # but can be a valid git reference in DOCKER_MODEL_REPO.
-DOCKER_MODEL_REF   ?= v1.0.1
+DOCKER_MODEL_REF   ?= v0.1.42
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
 # verify step. Leave empty or use any other value to install from download.docker.com
