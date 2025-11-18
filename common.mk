dd6df588f0b149005195392b378e52ba6d68cd11
Teddysun <i@teddysun.com>
2025-11-18 10:12:09 +0900

    Bump Docker version to v29.0.2 Bump buildx version to v0.30.1
    Signed-off-by: Teddysun <i@teddysun.com>

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ DOCKER_GITCOMMIT:=abcdefg
 GO_VERSION:=1.25.4
 PLATFORM=Docker Engine - Community
 SHELL:=/bin/bash
-VERSION?=29.0.1
+VERSION?=29.0.2
 
 # DOCKER_CLI_REPO and DOCKER_ENGINE_REPO define the source repositories to clone
 # the source from. These can be overridden to build from a fork.
@@ -36,15 +36,15 @@ DOCKER_MODEL_REPO   ?= https://github.com/docker/model-runner.git
 # tagged a release with the same version.
 #
 # For other situations, specify DOCKER_CLI_REF and/or DOCKER_ENGINE_REF separately.
-REF                ?= docker-v29.0.1
-DOCKER_CLI_REF     ?= v29.0.1
+REF                ?= docker-v29.0.2
+DOCKER_CLI_REF     ?= v29.0.2
 DOCKER_ENGINE_REF  ?= $(REF)
 # DOCKER_COMPOSE_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_COMPOSE_REPO.
 DOCKER_COMPOSE_REF ?= v2.40.3
 # DOCKER_BUILDX_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_BUILDX_REPO.
-DOCKER_BUILDX_REF  ?= v0.30.0
+DOCKER_BUILDX_REF  ?= v0.30.1
 # DOCKER_MODEL_REF is the version of model to package. It is usually a tag,
 # but can be a valid git reference in DOCKER_MODEL_REPO.
 DOCKER_MODEL_REF   ?= cmd/cli/v1.0.0
