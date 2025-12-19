6c602aac6cf0c7565923b818c06bc01a9a239463
Teddysun <i@teddysun.com>
2025-12-19 10:58:48 +0900

    Bump compose version to v5.0.1 Bump model version to v1.0.6
    Signed-off-by: Teddysun <i@teddysun.com>

--- common.mk
+++ common.mk
@@ -41,14 +41,14 @@ DOCKER_CLI_REF     ?= v29.1.3
 DOCKER_ENGINE_REF  ?= $(REF)
 # DOCKER_COMPOSE_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_COMPOSE_REPO.
-DOCKER_COMPOSE_REF ?= v5.0.0
+DOCKER_COMPOSE_REF ?= v5.0.1
 # DOCKER_BUILDX_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_BUILDX_REPO.
 DOCKER_BUILDX_REF  ?= v0.30.1
 # DOCKER_MODEL_REF is the version of model to package. It is usually a tag,
 # but can be a valid git reference in DOCKER_MODEL_REPO.
-DOCKER_MODEL_REF   ?= cmd/cli/v1.0.4
-DOCKER_MODEL_VER   ?= v1.0.4
+DOCKER_MODEL_REF   ?= cmd/cli/v1.0.6
+DOCKER_MODEL_VER   ?= v1.0.6
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
 # verify step. Leave empty or use any other value to install from download.docker.com
