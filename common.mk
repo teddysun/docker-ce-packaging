2786cb3ada83b54a00ec562bf66c6fa99c61079d
Teddysun <i@teddysun.com>
2026-05-20 11:09:45 +0900

    Bump buildx version to v0.34.1
    Signed-off-by: Teddysun <i@teddysun.com>

--- common.mk
+++ common.mk
@@ -44,7 +44,7 @@ DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_COMPOSE_REF ?= v5.1.3
 # DOCKER_BUILDX_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_BUILDX_REPO.
-DOCKER_BUILDX_REF  ?= v0.34.0
+DOCKER_BUILDX_REF  ?= v0.34.1
 # DOCKER_MODEL_REF is the version of model to package. It is usually a tag,
 # but can be a valid git reference in DOCKER_MODEL_REPO.
 DOCKER_MODEL_REF   ?= v1.1.39
